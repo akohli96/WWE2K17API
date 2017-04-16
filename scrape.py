e2ff50b8c7ca42115dc50e3bc0c485dbfbd726f2
Ayush Kohli <ayushk96@hotmail.com>
2017-04-16 17:19:57 -0500

    List comprehension in getting data FTW

--- scrape.py
+++ scrape.py
@@ -14,6 +14,12 @@ Study pymongo and store locally
 """
 
 
+ATTRIBUTES = {"Name":None,
+              "Height":None,
+              "Weight":None,
+              "From":None,
+              "Signatures":[],
+              "Debut Game":None}
 
 
 ROSTERLINK = "https://www.thesmackdownhotel.com/wwe2k17/roster/"
@@ -27,7 +33,9 @@ ROSTER_SOUP = BeautifulSoup(ROSTER_DATA, "html.parser")
 
 for superstar in ROSTER_SOUP.find_all(class_='roster_section'):
     superstar_link = superstar.find("a")
-    print superstar_link.text
+    superstar_dict={}
+    print superstar_link.text, "SUPERSTAR"
+    superstar_dict['Name'] = superstar_link.text.strip("\n")
     print superstar_link.get('href')
 
     s_link= "https://www.thesmackdownhotel.com/" + superstar_link.get('href')
@@ -37,28 +45,18 @@ for superstar in ROSTER_SOUP.find_all(class_='roster_section'):
 
     #print SUPERSTAR_SOUP
     #print SUPERSTAR_SOUP.prettify()
+
+    tags = [(strong_tag.text.strip(":").strip(),strong_tag.next_sibling.strip()) for strong_tag in SUPERSTAR_SOUP.find_all('strong', limit=6)[1:]]
+
+    for tag in tags:
+        print tag
+    """
     for strong_tag in SUPERSTAR_SOUP.find_all('strong', limit=6)[1:]:
-        print  strong_tag.text, strong_tag.next_sibling
+        print strong_tag.text.strip(":").strip()
+        print strong_tag.next_sibling.strip()
+    """
+    print superstar_dict
 
 
-SAMPLE_STAR_LINK = "https://www.thesmackdownhotel.com/wwe2k17/roster/alberto-del-rio"
-
-SUPERSTAR_SOUP = BeautifulSoup(requests.get(SAMPLE_STAR_LINK).text, "html.parser")
-
-#print SUPERSTAR_SOUP.prettify()
-
-print SUPERSTAR_SOUP.find(class_='contentheading').get_text().strip()
-
-#http://stackoverflow.com/questions/23380171/using-beautifulsoup-extract-text-without-tags
-for strong_tag in SUPERSTAR_SOUP.find_all('strong', limit=6)[1:]:
-    print  strong_tag.text, strong_tag.next_sibling
-
-
-ATTRIBUTES = {"Name":None,
-              "Height":None,
-              "Weight":None,
-              "From":None,
-              "Signatures":[],
-              "Debut Game":None}
 
 print "DONE"
