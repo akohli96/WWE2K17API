a81dfb451df13b6568edd735c3d50ec84cb46228
Ayush Kohli <ayushk96@hotmail.com>
2017-08-19 14:43:50 -0500

    Incremental commit. Regex extraction of superstar information

--- scrape.py
+++ scrape.py
@@ -4,59 +4,35 @@ Script to scrape 2K17 data
 
 from bs4 import BeautifulSoup
 import requests
-
-
+import re
 
 """
 TODO:
-Refactor and reorganise code into classes
-Study pymongo and store locally
+Refactor into functions
+Use functional programming techniques
+Log instead of print
 """
-
-
-ATTRIBUTES = {"Name":None,
-              "Height":None,
-              "Weight":None,
-              "From":None,
-              "Signatures":[],
-              "Debut Game":None}
-
-
 ROSTERLINK = "https://www.thesmackdownhotel.com/wwe2k17/roster/"
+ROSTER_SOUP = BeautifulSoup(requests.get(ROSTERLINK).text, "html.parser")
 
+PATTERN="PROFILE(Height:)(.*)(Weight:)([0-9]+).*(From:?)(.*)(Signature Move.*:)(.*)(First WWE Game:)(.*)"
 
+for superstar_subset in ROSTER_SOUP.find_all(class_='roster_section'):
+    #print superstar_subset
+    for individual in superstar_subset:
+        #print individual
+        individual_link = "https://www.thesmackdownhotel.com/" + individual.get('href')
 
-ROSTER_DATA = requests.get(ROSTERLINK).text
+        SUPERSTAR_SOUP = BeautifulSoup(requests.get(individual_link).text, "html.parser")
+        print SUPERSTAR_SOUP.find(class_="contentheading").get_text().strip()
+        text_found=SUPERSTAR_SOUP.find(class_="rosterbio").get_text(strip=True)
 
-ROSTER_SOUP = BeautifulSoup(ROSTER_DATA, "html.parser")
+        try:
+            #print text_found
+            for number,pattern in enumerate(re.search(PATTERN,text_found).groups()):
+                print number,pattern
+            print "\n"
+        except Exception as e:
+            print e
+        
 
-
-for superstar in ROSTER_SOUP.find_all(class_='roster_section'):
-    superstar_link = superstar.find("a")
-    superstar_dict={}
-    print superstar_link.text, "SUPERSTAR"
-    superstar_dict['Name'] = superstar_link.text.strip("\n")
-    print superstar_link.get('href')
-
-    s_link= "https://www.thesmackdownhotel.com/" + superstar_link.get('href')
-    print s_link
-    SUPERSTAR_SOUP = BeautifulSoup(requests.get(s_link).text, "html.parser")
-    #SUPERSTAR_SOUP.find(class_='contentheading').get_text().strip()
-
-    #print SUPERSTAR_SOUP
-    #print SUPERSTAR_SOUP.prettify()
-
-    tags = [(strong_tag.text.strip(":").strip(),strong_tag.next_sibling.strip()) for strong_tag in SUPERSTAR_SOUP.find_all('strong', limit=6)[1:]]
-
-    for tag in tags:
-        print tag
-    """
-    for strong_tag in SUPERSTAR_SOUP.find_all('strong', limit=6)[1:]:
-        print strong_tag.text.strip(":").strip()
-        print strong_tag.next_sibling.strip()
-    """
-    print superstar_dict
-
-
-
-print "DONE"
---
+++ superstar.py
@@ -0,0 +1,9 @@
+class SuperStar(object):
+
+    def __init__(self, name, height, weight, billed_from, signatures, debut_game):
+        self.name = name
+        self.height = height
+        self.weight = weight
+        self.billed_from = billed_from
+        self.signatures = signatures
+        self.debut_game = debut_game
