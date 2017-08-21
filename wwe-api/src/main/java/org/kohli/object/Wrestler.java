6221cc502981bfcc06b29c7a16426d96a533ebe2
Ayush Kohli <ayushk96@hotmail.com>
2017-08-21 14:22:26 -0500

    Created Wrestler POJO

---
+++ wwe-api/src/main/java/org/kohli/object/Wrestler.java
@@ -0,0 +1,54 @@
+package  org.kohli.object;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Wrestler{
+
+    private List<String> signatures;
+    private String name;
+    private int weight;
+    private String from;
+    private String debutGame;
+    private float height;
+
+    public Wrestler(List<String> signatures,String name,int weight,String from,String debutGame,float height){
+        this.signatures=signatures;
+        this.name=name;
+        this.weight=weight;
+        this.from=from;
+        this.debutGame=debutGame;
+        this.height=height;
+    }
+
+    @JsonProperty("signatures")
+    public List<String> getSignatures(){
+        return signatures;
+    }
+
+    @JsonProperty("name")
+    public String getName(){
+        return name;
+    }
+
+    @JsonProperty("weight")
+    public int getWeight(){
+        return weight;
+    }
+
+    @JsonProperty("billed_from")
+    public String getFrom(){
+        return from;
+    }
+
+    @JsonProperty("debut_game")
+    public String getDebutGame(){
+        return debutGame;
+    }
+
+    @JsonProperty("height")
+    public float getHeight(){
+        return height;
+    }
+}
--- wwe-api/src/main/java/org/kohli/services/WrestlerServiceImpl.java
+++ wwe-api/src/main/java/org/kohli/services/WrestlerServiceImpl.java
@@ -1,5 +1,9 @@
 package org.kohli.services;
 
+import java.util.Map;
+import java.util.HashMap;
+import org.kohli.object.Wrestler;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.inject.Inject;
@@ -7,10 +11,12 @@ import javax.inject.Inject;
 public class WrestlerServiceImpl implements WrestlerService {
 
     private ObjectMapper objectMapper;
+    private Map<String,Wrestler> wrestlerMap;
 
     @Inject
     public WrestlerServiceImpl(ObjectMapper objectMapper){
         this.objectMapper=objectMapper;
+        wrestlerMap = new HashMap<>();
         loadWrestlerData();
     }
 
