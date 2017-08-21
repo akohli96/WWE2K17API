8550542312cc6042c9f71107ca388a3f9bbe2454
Ayush Kohli <ayushk96@hotmail.com>
2017-08-20 21:02:17 -0500

    Scraped all data from wrestlers with all fields valid

--- constants.py
+++
@@ -1,6 +0,0 @@
-CONSTANTS = dict(
-    PATTERN="PROFILE(Height:)(.*)(Weight:)([0-9]+).*(From:?)(.*)(Signature Move.*:)(.*)(First WWE Game:)(.*)"
-    ,
-    ROSTERLINK = "https://www.thesmackdownhotel.com/wwe2k17/roster/",
-    NUMBERPATTERN="(\d)\D+(\d+).*"
-)
